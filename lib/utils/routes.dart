581bf5776a624eecadc5c928969c69ac958d47f8
Realzzy <utubeperyone2@gmail.com>
2024-09-26 08:10:10 +0700

    feat/route: Add new route for init and welcome page

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,25 @@
+import 'package:aify/pages/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-List<GetPage> pages = [];
+List<GetPage> pages = [
+  GetPage(
+      name: '/',
+      page: () {
+        Future.delayed(const Duration(seconds: 0), () async {
+          final supabase = Supabase.instance.client;
+          if (supabase.auth.currentUser != null) {
+            return Get.offAllNamed('/home');
+          }
+          return Get.offAllNamed('/welcome');
+        });
+        return const Scaffold();
+      }),
+  GetPage(
+    name: '/welcome',
+    page: () => const WelcomePage(),
+    transition: Transition.fadeIn,
+    transitionDuration: const Duration(milliseconds: 500),
+  ),
+];
